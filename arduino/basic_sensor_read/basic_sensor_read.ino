99227ac260eb43211815cba8e1f09f7e7d9fbcc2
Valtteri Erkkilä <valtteri.erkkila@gmail.com>
2017-11-28 15:01:34 +0200

    Do sensor calibration, print resistance ratios (of gassed and clean air)

--- arduino/basic_sensor_read/basic_sensor_read.ino
+++ arduino/basic_sensor_read/basic_sensor_read.ino
@@ -5,31 +5,87 @@
 #define SENSOR_A2 2
 #define SENSOR_A3 3
 
-float sensor_values[4] = {};
-float sensor_voltages[4] = {};
+#define NUM_CALIBRATION_READS 100
+#define TIME_BETWEEN_READS_MS 1000
 
-void setup() {
-  Serial.begin(9600);
+static float R0[4] = {};
+
+float get_voltage(float sensor_value)
+{
+  return sensor_value/1024.f*5.f;
 }
 
-void loop() {
+float get_resistance(float sensor_voltage)
+{
+  return (5.0 - sensor_voltage) / sensor_voltage;
+}
+
+void setup()
+{
+  float sensor_averages[4] = {0.f, 0.f, 0.f, 0.f};
+  float RS_air[4] = {0.f, 0.f, 0.f, 0.f};
+  
+  Serial.begin(9600);
+
+  /*Calibrate sensors by measuring the resistance of clean air*/
+  for (int i = 0; i < NUM_CALIBRATION_READS; ++i) {
+    sensor_averages[SENSOR_A0] += analogRead(SENSOR_A0);
+    sensor_averages[SENSOR_A1] += analogRead(SENSOR_A1);
+    sensor_averages[SENSOR_A2] += analogRead(SENSOR_A2);
+    sensor_averages[SENSOR_A3] += analogRead(SENSOR_A3);
+  }
+  sensor_averages[SENSOR_A0] /= NUM_CALIBRATION_READS;
+  sensor_averages[SENSOR_A1] /= NUM_CALIBRATION_READS;
+  sensor_averages[SENSOR_A2] /= NUM_CALIBRATION_READS;
+  sensor_averages[SENSOR_A3] /= NUM_CALIBRATION_READS;
+
+  RS_air[SENSOR_A0] = get_resistance(get_voltage(sensor_averages[SENSOR_A0]));
+  RS_air[SENSOR_A1] = get_resistance(get_voltage(sensor_averages[SENSOR_A1]));
+  RS_air[SENSOR_A2] = get_resistance(get_voltage(sensor_averages[SENSOR_A2]));
+  RS_air[SENSOR_A3] = get_resistance(get_voltage(sensor_averages[SENSOR_A3]));
+
+  /*R0 = resistance in clean air*/
+  R0[SENSOR_A0] = RS_air[SENSOR_A0]/9.8f;
+  R0[SENSOR_A1] = RS_air[SENSOR_A1]/9.8f;
+  R0[SENSOR_A2] = RS_air[SENSOR_A2]/9.8f;
+  R0[SENSOR_A3] = RS_air[SENSOR_A3]/9.8f;
+
+  Serial.print("Calculated R0 values: ");
+  Serial.print(R0[SENSOR_A0]);
+  Serial.print(" | ");
+  Serial.print(R0[SENSOR_A1]);
+  Serial.print(" | ");
+  Serial.print(R0[SENSOR_A2]);
+  Serial.print(" | ");
+  Serial.print(R0[SENSOR_A3]);
+  Serial.print("\n");
+}
+
+void loop()
+{
+  int sensor_values[4] = {0, 0, 0, 0};
+  float ratios[4] = {0.f, 0.f, 0.f, 0.f};
+
   sensor_values[SENSOR_A0] = analogRead(SENSOR_A0);
   sensor_values[SENSOR_A1] = analogRead(SENSOR_A1);
   sensor_values[SENSOR_A2] = analogRead(SENSOR_A2);
   sensor_values[SENSOR_A3] = analogRead(SENSOR_A3);
-  
-  sensor_voltages[SENSOR_A0] = sensor_values[SENSOR_A0]/1024.f*5.0f;
-  sensor_voltages[SENSOR_A1] = sensor_values[SENSOR_A1]/1024.f*5.0f;
-  sensor_voltages[SENSOR_A2] = sensor_values[SENSOR_A2]/1024.f*5.0f;
-  sensor_voltages[SENSOR_A3] = sensor_values[SENSOR_A3]/1024.f*5.0f;
-  
-  Serial.print(sensor_voltages[SENSOR_A0]);
+
+  /*Ratio of "contaminated" air to clean air*/
+  ratios[SENSOR_A0] = get_resistance(get_voltage(sensor_values[SENSOR_A0])) / R0[SENSOR_A0];
+  ratios[SENSOR_A1] = get_resistance(get_voltage(sensor_values[SENSOR_A1])) / R0[SENSOR_A1];
+  ratios[SENSOR_A2] = get_resistance(get_voltage(sensor_values[SENSOR_A2])) / R0[SENSOR_A2];
+  ratios[SENSOR_A3] = get_resistance(get_voltage(sensor_values[SENSOR_A3])) / R0[SENSOR_A3];
+
+  Serial.print("Ratios: ");
+  Serial.print(ratios[SENSOR_A0]);
   Serial.print(" | ");
-  Serial.print(sensor_voltages[SENSOR_A1]);
+  Serial.print(ratios[SENSOR_A1]);
   Serial.print(" | ");
-  Serial.print(sensor_voltages[SENSOR_A2]);
+  Serial.print(ratios[SENSOR_A2]);
   Serial.print(" | ");
-  Serial.print(sensor_voltages[SENSOR_A3]);
+  Serial.print(ratios[SENSOR_A3]);
   Serial.print("\n");
-  delay(1000);
+  
+  delay(TIME_BETWEEN_READS_MS);
 }
