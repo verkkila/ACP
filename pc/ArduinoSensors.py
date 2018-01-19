ae1ae16cff83089c2f7c0fda467c70549ebd70da
Valtteri Erkkila <valtteri.erkkila@gmail.com>
2018-01-19 09:41:29 +0200

    Add boolean returns for ArduinoSensors.open()

--- pc/ArduinoSensors.py
+++ pc/ArduinoSensors.py
@@ -17,18 +17,23 @@ class ArduinoSensors(Thread):
 
     def open(self):
         if "-s" in sys.argv:
-            self._serial_port = serial.Serial("/tmp/socatout", 9600, timeout = 3)
+            try:
+                self._serial_port = serial.Serial("/tmp/socatout", 9600, timeout = 3)
+            except serial.serialutil.SerialException:
+                print("Failed to open /tmp/socatout, is the virtual device running?")
+                return False
         else:
             try:
                 port = next(list_ports.grep("Arduino"))
             except StopIteration:
                 print("Arduino not found!")
-                return
+                return False
             else:
                 self._serial_port = serial.Serial(port.device, 9600, timeout = 3)
         self._running = True
         self.start()
         print("Serial port opened and poll thread running")
+        return True
 
     def close(self):
         self._running = False
@@ -42,33 +47,61 @@ class ArduinoSensors(Thread):
         while self._running:
             line = self._serial_port.readline()
             line = line.rstrip(b"\n")
-            data_floats = array.array("f", line)
             try:
+                data_floats = array.array("f", line)
+                self._sensor_lock.acquire(True)
                 self._sensor_0_value = data_floats[0]
                 self._sensor_1_value = data_floats[1]
                 self._sensor_2_value = data_floats[2]
                 self._sensor_3_value = data_floats[3]
+                self._sensor_lock.release()
+            except ValueError:
+                print("Failed to convert line: {}(len: {})".format(line, len(line)))
+                pass
             except IndexError:
                 pass
+        self._stop_event.set()
 
-    def get_front(self):
-        return self._sensor_2_value
+    def get_front(self, block_until_ready=True):
+        if self._sensor_lock.acquire(block_until_ready):
+            retval = self._sensor_2_value
+            self._sensor_lock.release()
+            return retval
+        else:
+            return 0.0
 
-    def get_back(self):
-        return self._sensor_0_value
+    def get_back(self, block_until_ready=True):
+        if self._sensor_lock.acquire(block_until_ready):
+            retval = self._sensor_0_value
+            self._sensor_lock.release()
+            return retval
+        else:
+            return 0.0
 
-    def get_left(self):
-        return self._sensor_1_value
+    def get_left(self, block_until_ready=True):
+        if self._sensor_lock.acquire(block_until_ready):
+            retval = self._sensor_1_value
+            self._sensor_lock.release()
+            return retval
+        else:
+            return 0.0
 
-    def get_right(self):
-        return self._sensor_3_value
+    def get_right(self, block_until_ready=True):
+        if self._sensor_lock.acquire(block_until_ready):
+            retval = self._sensor_3_value
+            self._sensor_lock.release()
+            return retval
+        else:
+            return 0.0
 
 sensors = ArduinoSensors()
-sensors.open()
-for i in range(0, 4):
-    time.sleep(2)
-    print("Front: {}".format(sensors.get_front()))
-    print("Back:  {}".format(sensors.get_back()))
-    print("Left:  {}".format(sensors.get_left()))
+if not sensors.open():
+    sys.exit()
+time.sleep(1)
+for i in range(0, 10):
+    time.sleep(1)
+    print("Front: {}".format(sensors.get_front()), end=" ")
+    print("Back:  {}".format(sensors.get_back()), end=" ")
+    print("Left:  {}".format(sensors.get_left()), end=" ")
     print("Right: {}".format(sensors.get_right()))
 sensors.close()
